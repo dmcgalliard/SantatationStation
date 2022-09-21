3a77552578fe2f33ea136c87a70a250436a144ad
Drew McGalliard <dsmcgalliard@gmail.com>
2022-09-21 11:49:54 -0500

    Add owens marker

--- Main.java
+++ Main.java
@@ -4,11 +4,18 @@ public class Main {
         Sanatizer s1 = new Sanatizer(); 
         Sanatizer s2 = new Sanatizer("Germ-X", 0.8);
         Sanatizer s3 = new Sanatizer("Solimo", 0.5, true);
+        Marker m1 = new Marker(); 
+        Sanatizer s4 = new Sanatizer(m1);
+
+
+
 
 
         System.out.println(s1); 
         System.out.println(s2); 
         System.out.println(s3);
+        System.out.println(); 
+        s4.mDispense();
     }
 
   
